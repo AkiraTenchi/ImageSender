03d1eb36434095698269491a3c0793c7f4ae9f58
unknown <detektivconan18@gmx.de>
2020-10-22 23:32:09 +0200

    Version 2

--- ImageSender.py
+++ ImageSender.py
@@ -1,7 +1,9 @@
 import discord
 import os
+import random
 
 client = discord.Client()
+random.seed(69)
 
 @client.event
 async def on_ready():
@@ -12,10 +14,48 @@ async def on_message(message):
     if message.author == client.user:
         return
     print('Message from {0.author}: {0.content}'.format(message))
+
+    if message.content.startswith('$help'):
+        await message.channel.send("Currently available: $trap, $neko")
     
-    if message.content.startswith('$images'):
-        images = os.listdir('./images')
-        for image in images:
-            await message.channel.send(file=discord.File(os.path.abspath('images') + "\\" + image))
+    if message.content.startswith('$trap'):
+        images = os.listdir('./traps')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./traps') + "\\" + image))
+        
+    if message.content.startswith('$lolineko'):
+        images = os.listdir('./lolisnekos')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./lolisnekos') + "\\" + image))
+
+    if message.content.startswith('$azurlane'):
+        images = os.listdir('./AzurLane')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./AzurLane') + "\\" + image))
+
+    if message.content.startswith('$genshin'):
+        images = os.listdir('./Genshin')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./Genshin') + "\\" + image))
+
+    if message.content.startswith('$girlsfrontline'):
+        images = os.listdir('./GirlsFrontline')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./GirlsFrontline') + "\\" + image))
+    
+    if message.content.startswith('$maids'):
+        images = os.listdir('./Maids')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./Maids') + "\\" + image))
+
+    if message.content.startswith('$rena'):
+        images = os.listdir('./RuuyguuRena')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./RuuyguuRena') + "\\" + image))
+
+    if message.content.startswith('$touhou'):
+        images = os.listdir('./traps')
+        image = images[random.randrange(len(images))]
+        await message.channel.send(file=discord.File(os.path.abspath('./Touhou') + "\\" + image))
 
 client.run('Enter The Token Here')
